88f0e391423bd5b775fbd77d513e14c19cb601c0
Jeremy Stotts <jeremystotts123@gmail.com>
2020-11-02 08:22:21 -0600

    Program Improvments
    Implimented a setup.py to compile and run scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AutoPartUpdater',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Vahalaru',
+    author_email='jeremystotts123@gmail.com',
+    description='Update Parts'
+)
